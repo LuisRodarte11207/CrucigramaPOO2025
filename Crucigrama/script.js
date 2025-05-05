5d27107a761133f372ecfdf1e4a8e245c99a855e
LuisRodarte11207 <luis.rodarte11207@gmail.com>
2025-05-05 15:49:49 -0600

    Delete Crucigrama directory

--- Crucigrama/script.js
+++
@@ -1,7 +0,0 @@
-function answ() {
-  if (confirm("OK para mostrar/Cancel para ocultar")) {
-    document.getElementById("answers").style = "display: initial";
-  } else {
-    document.getElementById("answers").style = "display: none";
-  }
-}
